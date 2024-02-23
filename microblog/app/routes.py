ac91283a989086425d6a7c917dbc9c747e49ac51
Jay <kred110396@gmail.com>
2024-02-24 00:39:47 +0800

    feat: add page navigation

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -30,7 +30,13 @@ def index():
         return redirect(url_for('index'))
     page = request.args.get('page', 1, type=int)
     posts = db.paginate(current_user.following_posts(), page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    return render_template('index.html', title='Home page', form=form, posts=posts.items)
+    
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+
+    return render_template('index.html', title='Home page', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/explore')
 @login_required
@@ -38,7 +44,13 @@ def explore():
     page = request.args.get('page', 1, type=int)
     query = sa.select(Post).order_by(Post.timestamp.desc())
     posts = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    return render_template('index.html', title='Explore', posts=posts.items)
+
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
