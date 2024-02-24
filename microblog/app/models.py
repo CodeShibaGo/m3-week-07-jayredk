bd7e52d16f26430b1a6b351da54f346bf3120e85
Jay <kred110396@gmail.com>
2024-02-24 21:55:02 +0800

    feat: add methods for reset password in User model

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -5,7 +5,9 @@ import sqlalchemy.orm as so
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
-from app import db, login
+from time import time
+import jwt
+from app import app, db, login
 
 @login.user_loader
 def load_user(id):
@@ -86,6 +88,21 @@ class User(UserMixin, db.Model):
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
+    
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode({
+            'reset_password': self.id, 'exp': time() + expires_in
+        }, app.config['SECRET_KEY'], algorithm='HS256')
+    
+    @staticmethod
+    def verify_reset_password_token(token):
+        print(token)
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)
+
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
